ee34823c478434c0e12ed69e469d735bdbd27fba
dclements9 <dclements@protonmail.com>
2019-11-08 15:06:43 -0500

    Initial root index & students component

---
+++ src/containers/StudentsList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class StudentsList extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1> Students List </h1>
+            </div>
+        )
+    }
+
+}
+
+export default StudentsList
