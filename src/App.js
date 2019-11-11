5c87d508539909f0d82fb70e29cfc66c5a26a46d
dclements9 <dclements@protonmail.com>
2019-11-11 12:22:40 -0500

    root app to class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 import Home from './components/Home'
 import UsersList from './containers/UsersList'
 import LessonsList from './containers/LessonsList'
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route path="/users" component={UsersList} />
-        <Route path="/lessons" component={LessonsList} />
-      </Router>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route path="/users" component={UsersList} />
+          <Route path="/lessons" component={LessonsList} />
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return ({
+    todos: state.mainUserReducer.users
+  })
+}
+
+export default connect(mapStateToProps)(App)
+
+// export default connect(mapStateToProps, {getStudents})(App)
--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
+
 let store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
 <Provider store={store}>
-    <App />,
+    <App />
 </Provider>, 
 document.getElementById('root')
 );
