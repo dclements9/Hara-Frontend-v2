d2798a80a249bd23586eca060ee2b30e1c17a2ac
dclements9 <dclements@protonmail.com>
2019-11-14 11:15:06 -0500

    split store to seperate file for readability

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers/index'
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-let store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+import store from './store'
 
 ReactDOM.render(
 <Provider store={store}>
