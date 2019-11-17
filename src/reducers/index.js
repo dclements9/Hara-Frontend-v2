d92e55c456ec79b57b2c2673a272dbc4903d1a75
dclements9 <dclements@protonmail.com>
2019-11-17 16:26:03 -0500

    cleaned up temp unneeded reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux'
-import userReducer from './userReducer'
-// import { getUser } from './userReducer'
-// import mainLessonReducer from './lessonReducer'
-import lessonReducer from './lessonReducer'
+import users from './userReducer'
+// import currentUser from './currentUser'
+// import loginForm from './loginFormReducer'
+import lessons from './lessonReducer'
 
 export default combineReducers ({
-    // mainUserReducer,
-    // mainLessonReducer
-    // user: getUser,
-    users: userReducer,
-    lessons: lessonReducer
-
+    users,
+    lessons,
+    // currentUser,
+    // loginForm
 })
