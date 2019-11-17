28ec348f47bc010b57bce4ac8e61e301c96cee9b
dclements9 <dclements@protonmail.com>
2019-11-17 16:23:22 -0500

    date formatted & edit lesson link

--- src/components/LessonPage.js
+++ src/components/LessonPage.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { deleteLesson } from '../actions/lessons'
 import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom'
 
 class LessonPage extends Component  {    
 
-    delete = () => {
-        this.props.deleteLesson(this.props.lesson.id, this.props.history)
-    }
+  formatDate(date) {
+      let dateParts = date.split('-');
+      let dateJoined = new Date (dateParts[0], (dateParts[1] - 1), dateParts[2])
+      let formattedDate= dateJoined.toDateString()
+      
+      return (formattedDate)
+  }
+
+  delete = () => {
+    this.props.deleteLesson(this.props.lesson.id, this.props.history)
+  }
 
 render (){
     const { lesson } = this.props
@@ -19,17 +28,22 @@ render (){
     <div> 
       <h1> {lesson.title} </h1>
       <h3> {lesson.description} </h3>
-      <p>  {lesson.date}</p>
+      <p> {this.formatDate(lesson.date)}</p>
       <p>  {new Date(lesson.start_time).toLocaleString("en-US", {timeZone: "UTC", hour: '2-digit', minute:'2-digit'})} - 
            {new Date(lesson.end_time).toLocaleString("en-US", {timeZone: "UTC", hour: '2-digit', minute:'2-digit'})}</p>
       <Button onClick={this.delete}> Delete </Button>
+      <br />
+      <br />
+      <Link to={`/lessons/${lesson.id}/edit`}> Update </Link>
+      <br />
+      <br />
     </div>
     )}
 }
-const mapStateToProps = (state, props) => {
-  const id = props.match.params.id;
-  const lesson = state.lessons.filter(lesson => lesson.id === parseInt(id))[0]
-  return { lesson }
-}
+  const mapStateToProps = (state, props) => {
+    const id = props.match.params.id;
+    const lesson = state.lessons.filter(lesson => lesson.id === parseInt(id))[0]
+    return { lesson }
+  }
 
 export default connect(mapStateToProps, { deleteLesson })(LessonPage)
