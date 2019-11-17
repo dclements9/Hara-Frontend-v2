877e74104428af8f6730267a382552435b5acf78
dclements9 <dclements@protonmail.com>
2019-11-16 21:24:09 -0500

    lessonPage -> class & delete button added

--- src/components/LessonPage.js
+++ src/components/LessonPage.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { deleteLesson } from '../actions/lessons'
 
-function LessonPage (props) {    
-  if (!props.lesson){
+class LessonPage extends Component  {    
+
+    delete = () => {
+        this.props.deleteLesson(this.props.lesson.id)
+    }
+
+render (){
+    const { lesson } = this.props
+  if (!lesson){
     return <p> Loading... </p>
   }
 
   return (
-    
     <div> 
-      <h1> {props.lesson.title} </h1>
-      <h3> {props.lesson.description} </h3>
-      <p>{props.lesson.date}</p>
-      <p>{new Date(props.lesson.start_time).toLocaleString("en-US", {timeZone: "UTC", hour: '2-digit', minute:'2-digit'})} - 
-      {new Date(props.lesson.end_time).toLocaleString("en-US", {timeZone: "UTC", hour: '2-digit', minute:'2-digit'})}</p>
+      <h1> {lesson.title} </h1>
+      <h3> {lesson.description} </h3>
+      <p>  {lesson.date}</p>
+      <p>  {new Date(lesson.start_time).toLocaleString("en-US", {timeZone: "UTC", hour: '2-digit', minute:'2-digit'})} - 
+           {new Date(lesson.end_time).toLocaleString("en-US", {timeZone: "UTC", hour: '2-digit', minute:'2-digit'})}</p>
+      <Button onClick={this.delete}> Delete </Button>
     </div>
-  );
+    )}
 }
-
 const mapStateToProps = (state, props) => {
     const id = props.match.params.id;
     const lesson = state.lessons.filter(lesson => lesson.id == id)[0]
     return { lesson }
 }
 
-export default connect(mapStateToProps)(LessonPage)
+export default connect(mapStateToProps, { deleteLesson })(LessonPage)
