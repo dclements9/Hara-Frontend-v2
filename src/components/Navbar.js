68f281bd526c1b1b7927112d06d190e3ab3ab213
dclements9 <dclements@protonmail.com>
2019-11-17 22:09:20 -0500

    Navbar with proper links and style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 
-const Navheader = () => {
+
+const NavBar = () => {
   return (
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand style={{ color: "#218838" }} href="/">
-        Hara
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>
+      <img
+        src="./logo.png"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="TKD logo"></img>
       </Navbar.Brand>
-      <Navbar.Brand style={{ color: "#218838" }} href="/users">
-        Users
-      </Navbar.Brand>
-      <Navbar.Brand style={{ color: "#218838" }} href="/lessons">
-        Lessons
-      </Navbar.Brand>
-      <Navbar.Brand style={{ color: "#218838" }} href="/challenges">
-        Challenges
-      </Navbar.Brand>
-      {/* {/* <Nav.Link href="/users" className="ml-auto"> */}
-      <Nav.Link href="/lessons" className="ml-auto">
-      </Nav.Link>
-      <Nav.Link href="/challenges" className="ml-auto"> */}
-      </Nav.Link>
-     
+      <Navbar.Brand style={{ color: "#5299d3" }} href="/"> Hara </Navbar.Brand>
+      <Navbar.Brand style={{ color: "#5299d3" }} href="/users"> Users </Navbar.Brand>
+      <Navbar.Brand style={{ color: "#5299d3" }} href="/lessons"> Lessons </Navbar.Brand>
+      <Navbar.Brand style={{ color: "#5299d3" }} href="/challenges"> Challenges </Navbar.Brand>    
     </Navbar>
   );
 };
 
 
-export default connect()(Navheader);
+export default connect()(NavBar);
