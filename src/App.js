8814c47e3f03c2ec9efe4c8d9ea1348c07a30461
Dylan Clements <dclements@protonmail.com>
2019-11-17 19:14:50 -0500

    Challenges routes

--- src/App.js
+++ src/App.js
@@ -6,21 +6,27 @@ import NavHeader from "./components/Navbar";
 // import Home from './components/Home'
 import NewLesson from './containers/NewLesson'
 import NewUser from './containers/NewUser'
+import NewChallenge from './containers/NewChallenge'
 import UsersList from './containers/UsersList'
 import LessonsList from './containers/LessonsList'
+import ChallengesList from './containers/ChallengesList'
 import Welcome from './components/Welcome'
 import { getLessons } from './actions/lessons'
 import { getUsers } from './actions/users'
+import { getChallenges } from './actions/challenges'
 import LessonPage from './components/LessonPage'
 import UserPage from './components/UserPage'
+import ChallengePage from './components/ChallengePage'
 import EditUser from './containers/EditUser.js'
-import EditLesson from './containers/EditLesson';
+import EditLesson from './containers/EditLesson'
+import EditChallenge from './containers/EditChallenge'
 
 class App extends Component {
 
   componentDidMount() {
     this.props.getLessons();
     this.props.getUsers();
+    this.props.getChallenges();
   }
 
   render() {
@@ -31,17 +37,20 @@ class App extends Component {
         <NavHeader />
           {/* <Route exact path="/" component={Home} /> */}
           <Route exact path="/" component={Welcome} />
+          <Route path="/welcome" component={Welcome} />
           <Route exact path="/users" component={UsersList} />
           <Route exact path="/lessons" component={LessonsList} />
+          <Route exact path="/challenges" component={ChallengesList} />
           <Route exact path="/lessons/new" component={NewLesson} />
           <Route exact path="/users/new" component={NewUser} />
-          <Route path="/welcome" component={Welcome} />
+          <Route exact path="/challenges/new" component={NewChallenge} />
           <Route path="/lessons/:id" component={LessonPage}/>
           <Route path="/lessons/:id/edit" component={EditLesson}/>
           <Route path="/users/:id" component={UserPage} />
           <Route path="/users/:id/edit" component={EditUser} />
+          <Route path="/challenges/:id" component={ChallengePage} />
+          <Route path="/challenges/:id/edit" component={EditChallenge} />
         </Router>
-        
       </div>
     );
   }
@@ -50,8 +59,9 @@ class App extends Component {
 const mapStateToProps = state => {
   return {
     lessons: state.lessons,
-    users: state.users
+    users: state.users,
+    challenges: state.challenges
   };
 };
 
-export default connect(mapStateToProps, { getLessons, getUsers })(App);
+export default connect(mapStateToProps, { getLessons, getUsers, getChallenges })(App);
