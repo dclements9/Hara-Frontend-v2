9e193f5adbb3b91a28bc65903167279437b52e90
dclements9 <dclements@protonmail.com>
2019-11-20 08:22:03 -0500

    Button route to new user

--- src/containers/UsersList.js
+++ src/containers/UsersList.js
@@ -4,14 +4,28 @@ import { Link } from "react-router-dom"
 import Button from 'react-bootstrap/Button'
 
 class UsersList extends Component {
+    constuctor() {
+        this.routeChange = this.routeChange.bind(this);
+      }
+
+    routeChange(e) {
+        let path = `/users/new`;
+        // debugger;
+        this.props.history.push(path);
+      }
+
     render() {
         if (this.props.users.length === 0) {
             // Possible loading bar/cycle
             return <h1> Loading... </h1>
         }
+        const divStyle = {
+            color:'white'
+        }
+
         return (
             // TODO: Rank conversion
-            <div>
+            <div style={{ color:'white'}}>
                 <h1> Users List </h1>
 
                 {this.props.users.map( user => 
@@ -21,7 +35,7 @@ class UsersList extends Component {
                     </div>
                 )}
                 <div>
-                    <Button href={"/users/new"}><h3>Create New User</h3></Button>
+                    <Button onClick={(e) => {this.routeChange(e)}}><h3>Create New User</h3></Button>
                 </div>
             </div>
         )
