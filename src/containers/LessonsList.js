c4d98b07daa5ab109bdaad5789fa78c4388bb189
dclements9 <dclements@protonmail.com>
2019-11-20 18:45:17 -0500

    added spinner and getLessons action

--- src/containers/LessonsList.js
+++ src/containers/LessonsList.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from "react-router-dom"
 import Button from 'react-bootstrap/Button'
-
+import Spinner from 'react-bootstrap/Spinner'
+import { getLessons } from '.././actions/lessons'
 
 class LessonsList extends Component {
+
+    componentDidMount() {
+        this.props.getLessons();
+    }
 
 formatDate(date) {
     let dateParts = date.split('-');
@@ -16,7 +21,9 @@ formatDate(date) {
 
     render() {
         if (this.props.lessons.length === 0) {
-            return <h1> Loading... </h1>
+            return <Spinner animation="grow" variant="primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
         }
 
         return (
@@ -41,4 +48,4 @@ formatDate(date) {
 
 }
 
-export default connect(state => ({ lessons: state.lessons }))(LessonsList)
+export default connect(state => ({ lessons: state.lessons }), { getLessons })(LessonsList)
