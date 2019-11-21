f01d64564a9ae9a4b4d8495e1df43335a2a02714
dclements9 <dclements@protonmail.com>
2019-11-21 16:25:02 -0500

    Form styling and requirements

--- src/containers/NewUser.js
+++ src/containers/NewUser.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { createUser} from '../actions/users'
+import { Button, Form, FormControl, Col, Row } from 'react-bootstrap'
 
 class NewUser extends Component {
     state = {
@@ -23,27 +24,62 @@ class NewUser extends Component {
     render() {
         return (
             <div>
-                <div>
-                    {console.log(this.props.history)}
-                </div>
+                <br />
+                <br />
                 <form onSubmit = {this.handleSubmit}>
-                    <label> First Name: </label>
-                    <input required type="text" name= "first_name" value={this.state.first_name} onChange = {this.handleChange} />
-                    <label> Last Name: </label>
-                    <input required type="text" name= "last_name" value={this.state.last_name} onChange = {this.handleChange}/>
-                    <br />
-                    <br />
-                    <label> Email: </label>
-                    <input required type="text" name="email" value={this.state.email} onChange = {this.handleChange}/>
-                    <br />
-                    <br />
-                    <label> Age: </label>
-                    <input required type="text" name="age" value={this.state.age} onChange = {this.handleChange}/>
-                    <label> Rank: </label>
-                    <input required type="text" name="rank" value={this.state.rank} onChange = {this.handleChange}/>
-                    <br />
-                    <br />
-                <input type="submit" value="Create User" />
+                    <Form.Group as={Row} controlId="nameGroup">
+                        <Form.Label column xs={1}>First Name:</Form.Label>
+                    <Col xs={2}>
+                        <Form.Control required type="textarea" rows="1" name="first_name" onChange={this.handleChange} />
+                    </Col>
+                    <Form.Label column xs={1}>Last Name:</Form.Label>
+                    <Col xs={2}>
+                        <Form.Control required type="textarea" rows="1" name="last_name" onChange={this.handleChange} />
+                    </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="formHorizontalEmail">
+                        <Form.Label column xs={1}>Email:</Form.Label>
+                    <Col xs={4}>
+                        <Form.Control required type="email" placeholder="name@example.com" name="email" onChange={this.handleChange}/>
+                    </Col>
+                    </Form.Group>
+                    
+                    <Form.Group as={Row} controlId="ageGroup">
+                        <Form.Label column xs={1}>Age:</Form.Label>
+                    <Col xs={1}>
+                        <Form.Control required type="number" rows="1" name="age" onChange={this.handleChange} />
+                    </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="rankSelect">
+                    <Form.Label column xs={1}>Select Rank</Form.Label>
+                    <Col xs={2}>
+                        <FormControl required as="select" name="rank" onChange={this.handleChange}>
+                        <option value="">Select Rank</option>
+                        <option value="9th kup">9th kup</option>
+                        <option value="8th kup">8th kup</option>
+                        <option value="7th kup">7th kup</option>
+                        <option value="6th kup">6th kup</option>
+                        <option value="5th kup">5th kup</option>
+                        <option value="4th kup">4th kup</option>
+                        <option value="3rd kup">3rd kup</option>
+                        <option value="2nd kup">2nd kup</option>
+                        <option value="1st kup">1st kup</option>
+                        <option value="1st dan">1st dan</option>
+                        <option value="2nd dan">2nd dan</option>
+                        <option value="3rd dan">3rd dan</option>
+                        <option value="4th dan">4th dan</option>
+                        <option value="5th dan">5th dan</option>
+                        </FormControl>
+                        </Col>
+                    </Form.Group>
+                    
+                <Form.Group as={Row}>
+                    <Col sm={{ span: 4, offset: 0 }}>
+                    <Button type="submit">Submit</Button>
+                    </Col>
+                </Form.Group>
                 </form>
             </div>
         )
