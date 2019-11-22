b747b1225ca30dbcb0d2866555d6cd638e6df4e1
Dylan Clements <dclements@protonmail.com>
2019-11-22 11:24:45 -0500

    init search component structure (WIP)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Search from './Search'
+import Spinner from 'react-bootstrap/Spinner'
+import { getUsers } from '.././actions/users'
 
 class Home extends Component {
 
+    componentDidMount() {
+        this.props.getUsers();
+    }
+    
     render() {
+        if (this.props.users.length === 0) {
+            return <Spinner animation="grow" variant="primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+        }
         return (
             <div>
                 <h1> Welcome Student</h1>
                 <h3> Search for your name to sign in:</h3>
-                <Search />
+                <Search users={this.props.users}/>
             </div>
         )
     }
 }
 
-export default Home
+export default connect(state => ({ users: state.users }), {getUsers} )(Home)
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,31 +3,45 @@ import React, { Component } from 'react'
 class Search extends Component {
     state = {
       query: '',
-      results: []
+      results: [],
+      searchTerm: ''
     }
   
- handleInputChange = () => {
-    this.setState({
-      query: this.search.value
-    }, () => {
-      if (this.state.query && this.state.query.length > 1) {
-        if (this.state.query.length % 2 === 0) {
-            //Async Call
-        //   this.getInfo()
-        }
-      } 
-    })
-  }
+
+handleChange = (e) => {
+  this.setState({ [e.target.name]: e.target.value })
+}
+
+handleSubmit = (e) => {
+  e.preventDefault()
+  debugger;
+  // Get List - API Call?
+}
+
+
+//  handleInputChange = () => {
+//     this.setState({
+//       query: this.search.value
+//     }, () => {
+//       if (this.state.query && this.state.query.length > 1) {
+//         if (this.state.query.length % 2 === 0) {
+//           debugger;
+//             //Async Call
+//         //   this.getInfo()
+//         }
+//       } 
+//     })
+//   }
 
   render() {
     return (
-      <form>
-        <input
-          placeholder="Search for..."
-          ref={input => this.search = input}
-          onChange={this.handleInputChange}
-        />
-        <p>{this.state.query}</p>
+      <form onSubmit = {this.handleSubmit}>
+        {/* <input placeholder="Search for..." ref={input => this.search = input} onChange={this.handleInputChange}/>
+        <p>{this.state.query}</p> */}
+
+        <input type="text" placeholder="Search" name="searchTerm" onChange={this.handleChange} />
+
+        <input type="submit">Search</input>
       </form>
     )
   }
