69bbb00cf553d5d97c2c356d42709b1b5e0dac2e
dclements9 <dclements@protonmail.com>
2019-11-23 11:04:24 -0500

    UsersList to Presentational/Container

---
+++ src/components/UsersListComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { LinkContainer, Button } from 'react-router-bootstrap'
+import { Link } from "react-router-dom"
+
+const UsersListComponent = props => (
+    
+<div>
+    <h1> Users List </h1>
+    <br />
+    {props.users.map( user => 
+        <div>
+            <Link to={`/users/${user.id}`}><h2>{user.first_name} {user.last_name}</h2></Link>
+            <p>rank: {user.rank}</p>
+        </div>
+    )}
+    
+        {/* <LinkContainer exact to="/users/new"><Button><h3>Create New User</h3></Button></LinkContainer> */}
+    
+</div>
+)
+export default UsersListComponent
---
+++ src/containers/UsersListContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getUsers } from '../actions/users'
+import Spinner from 'react-bootstrap/Spinner'
+import UsersListComponent from '.././components/UsersListComponent'
+
+class UsersListContainer extends Component {
+    componentDidMount() {
+        this.props.getUsers();
+    }
+
+    render() {
+        if (this.props.users.length === 0) {
+            return <Spinner animation="grow" variant="primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+        }
+        return (
+            <UsersListComponent users={this.props.users}/>
+        )
+    }
+}
+
+export default connect(state => ({ users: state.users }), {getUsers} )(UsersListContainer)
+
