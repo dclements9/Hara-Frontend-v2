34212fd643bf21ebfb7353b04b4be1a2a1f66ecb
dclements9 <dclements@protonmail.com>
2019-12-01 09:04:02 -0500

    User Check-In Button

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap'
+import { connect } from 'react-redux'
 
 class SearchResults extends Component {
     state = {
@@ -13,17 +15,16 @@ class SearchResults extends Component {
     render() {
         return (
             <div>
-                <div>{console.log(this.props)}</div>
+                <div>{console.log(this.props.currentLesson)}</div>
             {this.filterUsers(this.props).map( user => 
                 <div>
                     <h2>{user.first_name} {user.last_name}</h2>
                     <h3> rank: {user.rank} </h3>
+                    <Button variant="success"> Check-In </Button>
                 </div>
             )} 
             </div>
     )}
 }
 
-
-
-export default SearchResults
+export default connect(state => ({ currentLesson: state.currentLesson }))(SearchResults)
