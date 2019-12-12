9e80109a84de60879690ec5782c75cc74019f168
Dylan Clements <dclements@protonmail.com>
2019-12-12 08:53:33 -0500

    currentLesson - USER_ASSOCIATED reducer

--- src/reducers/currentLessonReducer.js
+++ src/reducers/currentLessonReducer.js
@@ -1,7 +1,9 @@
 export default (state = [], action) => {
     switch (action.type) {
 case 'SET_CURRENT_LESSON':
-            return action.lesson
+    return action.lesson
+case 'USER_ASSOCIATED':
+    return action.payload
 default:
     return state
     }
