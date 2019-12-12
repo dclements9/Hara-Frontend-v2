edde9b0b325d18cf9145e3b60b97384aa49f3bd6
Dylan Clements <dclements@protonmail.com>
2019-12-12 08:54:15 -0500

    mapped associateUser to dispatch & attach user function

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,30 +1,37 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap'
 import { connect } from 'react-redux'
+import { associateUser } from '../actions/currentLesson'
 
 class SearchResults extends Component {
     state = {
         selectedUser: ''
     }
+
     filterUsers( props){
         return props.users.filter(user => user.last_name === props.searchTerm) 
     }
 
-//TODO: User to Lesson - fetch POST request to associate.
+    attachUser = (user) => {
+        let updatedLesson = this.props.currentLesson
+        
+        updatedLesson.users = updatedLesson.users.concat(user)
+        this.props.associateUser(updatedLesson, updatedLesson.id, this.props.history)
+    }
 
     render() {
         return (
             <div>
-                <div>{console.log(this.props.currentLesson)}</div>
             {this.filterUsers(this.props).map( user => 
                 <div>
                     <h2>{user.first_name} {user.last_name}</h2>
                     <h3> rank: {user.rank} </h3>
-                    <Button variant="success"> Check-In </Button>
+                    
+                    <Button variant="success" onClick={() => this.attachUser(user)}> Check-In </Button>
                 </div>
             )} 
             </div>
     )}
 }
 
-export default connect(state => ({ currentLesson: state.currentLesson }))(SearchResults)
+export default connect(state => ({ currentLesson: state.currentLesson }), {associateUser})(SearchResults)
