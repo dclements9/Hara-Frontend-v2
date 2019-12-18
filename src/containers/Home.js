0ddd957ededebc52009fe325e8a1597ca1ddd9b8
Dylan Clements <dclements@protonmail.com>
2019-12-18 14:03:32 -0500

    conditionally sets submit button disabled.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,14 +8,23 @@ import RetrieveCurrentLesson from './RetrieveCurrentLesson'
 import { Button } from 'react-bootstrap'
 
 class Home extends Component {
-    state = {
-      searchTerm: '',
-      isSubmitted: false
-    }
+  
+  state = {
+    searchTerm: '',
+    isSubmitted: false
+  }
+
 
 componentDidMount() {
   this.props.getUsers();
   this.props.getLessons()
+}
+
+isSubmitEnabled(){
+  if (this.props.currentLesson.title !== "No lessons today."){
+    return true
+  }
+  return false
 }
 
 handleChange = (e) => {
@@ -28,6 +37,8 @@ handleSubmit = (e) => {
 }
 
   render() {
+    const isEnabled = this.isSubmitEnabled();
+
     if (this.props.users.length === 0 || this.props.lessons.length === 0) {
       return <Spinner animation="grow" variant="primary" role="status">
                   <span className="sr-only"></span>
@@ -37,10 +48,12 @@ handleSubmit = (e) => {
     return (
       <div>
         <RetrieveCurrentLesson />
+
       <form onSubmit = {this.handleSubmit}>
         <input type="text" placeholder="Search" name="searchTerm" onChange={this.handleChange} />
-        <br />        
-        <Button type="submit">Search</Button>
+        <br />
+        
+        <Button type="submit" disabled={!isEnabled}>Search</Button>
       </form>
       <br />
         {this.state.isSubmitted && <SearchResults 
@@ -50,4 +63,4 @@ handleSubmit = (e) => {
   }
 }
 
-export default connect(state => ({ users: state.users, lessons: state.lessons }), {getUsers, getLessons} )(Home)
+export default connect(state => ({ users: state.users, lessons: state.lessons, currentLesson: state.currentLesson}), {getUsers, getLessons} )(Home)
